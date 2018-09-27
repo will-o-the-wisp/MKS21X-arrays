3667a12c3fdf99d2be04859eb5e3008d527fe394
will-o-the-wisp <kli18@stuy.edu>
2018-09-27 10:58:29 -0400

    created toString, main

--- arrayDemo.java
+++ arrayDemo.java
@@ -3,4 +3,18 @@ public class arrayDemo{
 	}
 	public static void printArray(int[][]ary){
 	}
+	public static String toString(int[]ary){
+		String ans = "{";
+		for(int i = 0;i < ary.length;i++){
+			ans += ary[i];
+			if (i < ary.length-1){
+				ans += ", ";
+			}
+		}
+		return ans+"}";
+	}
+	public static void main(String[] args) {
+		int[] ary = {1,2,3};
+		System.out.println(toString(ary));
+	}
 }
