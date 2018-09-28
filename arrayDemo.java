50ee608c42b8aa0c0f79672f493110a0a5ddc179
will-o-the-wisp <kli18@stuy.edu>
2018-09-27 21:08:09 -0400

    debugged fill2D

--- arrayDemo.java
+++ arrayDemo.java
@@ -1,10 +1,10 @@
 public class arrayDemo{
 	public static void printArray(int[]ary){
-		System.out.print(toString(ary));
+		System.out.println(toString(ary));
 	}
 	public static void printArray(int[][]ary){
 		for(int i = 0;i < ary.length;i++){
-			System.out.println(toString(ary));
+			System.out.println(toString(ary[i]));
 		}
 	}
 	public static String toString(int[]ary){
@@ -17,8 +17,34 @@ public class arrayDemo{
 		}
 		return ans+"}";
 	}
+	public static int countZeros2D(int[][] nums){
+		int ans = 0;
+		for(int i = 0;i < nums.length;i++){
+			for(int j = 0;j < nums[i].length;j++){
+				if(nums[i][j] == 0){
+					ans++;
+				}
+			}
+		}
+		return ans;
+	}
+	public static void fill2D(int[][] vals){
+		for(int i=0;i<vals.length;i++){
+			for(int j=0;j<vals[i].length;j++){
+				if(i==j){
+					vals[i][j]=3;
+				}
+				else vals[i][j]=1;
+			}
+		}
+	}
 	public static void main(String[] args) {
+		/*
 		int[] ary = {1,2,3};
 		printArray(ary);
+		*/
+		int[][] ary2 = {{1,0,3},{0,5,6,0,0}};
+		fill2D(ary2);
+		printArray(ary2);
 	}
 }
